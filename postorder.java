44235a7920b849d596759a85722579de5fc5a523
Parul Kohli <45730946+Parul1999@users.noreply.github.com>
2019-11-06 10:47:38 +0530

    Add files via upload
    postorder

---
+++ postorder.java
@@ -0,0 +1,8 @@
+void postorder(Node node) 
+    { 
+        if (node == null) 
+            return; 
+      postorder(node.left); 
+        postorder(node.right); 
+        System.out.print(node.data + " "); 
+    } 
